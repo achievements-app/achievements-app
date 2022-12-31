af389aa02800f681e9a331195be2849396ec29c9
Wes Copeland <wlcopeland1@gmail.com>
2022-12-30 21:16:36 -0500

    fix(retroachievements): ensure all calls are behind rate limiter

--- apps/api/src/app/shared/integrations/retroachievements/retroachievements-data.service.ts
+++ apps/api/src/app/shared/integrations/retroachievements/retroachievements-data.service.ts
@@ -7,12 +7,14 @@ import client from "./utils/retroachievements-client";
 export class RetroachievementsDataService {
   // SEE: https://github.dev/RetroAchievements/RAWeb/blob/ae5bf5e49246c0f50582177bdab9dd0e88f0a7d1/app/Api/RouteServiceProvider.php#L24-L25
   #rateLimiter = new RateLimiter({
-    tokensPerInterval: 1,
-    interval: "second"
+    tokensPerInterval: 60,
+    interval: "minute"
   });
 
   // SEE: https://github.dev/RetroAchievements/RAWeb/blob/master/public/API/API_GetUserCompletedGames.php
   async fetchAllUserGames(targetUserName: string) {
+    await this.#rateLimiter.removeTokens(1);
+
     const userCompletedGames = await client.getUserGameCompletionStats(
       targetUserName
     );
